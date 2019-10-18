77d15e0529805038de239e615db62e28615feb1a
wedcloud <xuhb@hitrobotgroup.com>
2019-10-18 22:42:56 +0800

    Swagger配置

--- src/main/java/club/wedcloud/www/WwwApplication.java
+++ src/main/java/club/wedcloud/www/WwwApplication.java
@@ -1,9 +1,12 @@
 package club.wedcloud.www;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableSwagger2Doc
 @SpringBootApplication
 @MapperScan("club.wedcloud.www.mapper")
 public class WwwApplication {
