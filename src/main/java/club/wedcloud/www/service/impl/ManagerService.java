c86b23f02a42a7cfa1cd34ac72165f8d52163f67
xuhb <xuhb@hitrobotgroup.com>
2019-11-25 17:06:48 +0800

    12

--- src/main/java/club/wedcloud/www/service/impl/ManagerService.java
+++ src/main/java/club/wedcloud/www/service/impl/ManagerService.java
@@ -1,50 +1,50 @@
 package club.wedcloud.www.service.impl;
 
-import club.wedcloud.www.dao.Manager;
-import club.wedcloud.www.service.JdbcTemplateService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import club.wedcloud.www.dao.Manager;
+import club.wedcloud.www.service.JdbcTemplateService;
 
 @Service
 public class ManagerService implements JdbcTemplateService<Manager> {
 
-    @Autowired
-    @Qualifier("weduserJdbcTemplate")
-    private JdbcTemplate jdbcTemplate;
+  @Autowired
+  @Qualifier("weduserJdbcTemplate")
+  private JdbcTemplate jdbcTemplate;
 
-    @Override
-    public List<Manager> getList() {
-        return jdbcTemplate.queryForList(
-                "SELECT `id`,`user_name` AS userName,`mobile`,`status`,`created_at` AS createdAt,`modified_at` AS modifiedAt FROM `manager` WHERE `deleted_at` IS NULL",
-                Manager.class);
-    }
+  @Override
+  public List<Manager> getList() {
+    return jdbcTemplate.query(
+        "SELECT `id`,`user_name` AS userName,`mobile`,`status`,`created_at` AS createdAt,`modified_at` AS modifiedAt FROM `manager` WHERE `deleted_at` IS NULL",
+        new BeanPropertyRowMapper(Manager.class));
+  }
 
-    @Override
-    public Manager getInfo(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override
+  public Manager getInfo(Integer id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public Integer addInfo(Manager t) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override
+  public Integer addInfo(Manager t) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public Integer updateInfo(Manager t) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override
+  public Integer updateInfo(Manager t) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public Integer deleteInfo(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+  @Override
+  public Integer deleteInfo(Integer id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
